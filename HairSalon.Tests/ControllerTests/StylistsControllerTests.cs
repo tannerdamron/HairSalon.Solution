23accbe03cb5a0d3677cce30eccb2318c619f251
Tanner Damron <tanner.mdamron@yahoo.com>
2019-03-01 11:03:16 -0800

    add controllers and corresponding views, and test controllers and pass

---
+++ HairSalon.Tests/ControllerTests/StylistsControllerTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class StylistsControllerTest
+    {
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            StylistsController controller = new StylistsController();
+            ActionResult indexView = controller.Index();
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void New_ReturnsCorrectView_True()
+        {
+            StylistsController controller = new StylistsController();
+            ActionResult newView = controller.New();
+            Assert.IsInstanceOfType(newView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Create_ReturnsCorrectView_True()
+        {
+            StylistsController controller = new StylistsController();
+            ActionResult newView = controller.Create("Jane");
+            Assert.IsInstanceOfType(newView, typeof(RedirectToActionResult));
+        }
+
+        [TestMethod]
+        public void Show_ReturnsCorrectView_True()
+        {
+            StylistsController controller = new StylistsController();
+            ActionResult newView = controller.Show(0);
+            Assert.IsInstanceOfType(newView, typeof(ViewResult));
+        }
+    }
+}
---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class StylistsController : Controller
+    {
+        [HttpGet("/stylists")]
+        public ActionResult Index()
+        {
+            List<Stylist> allstylists = Stylist.GetAll();
+            return View(allstylists);
+        }
+
+        [HttpGet("/stylists/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("/stylists")]
+        public ActionResult Create(string stylistName)
+        {
+            Stylist newStylist = new Stylist(stylistName);
+            newStylist.Save();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/stylists/{id}")]
+        public ActionResult Show(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Stylist selectedStylist = Stylist.Find(id);
+            List<Client> stylistClients = selectedStylist.GetClients();
+            model.Add("stylist", selectedStylist);
+            model.Add("clients", stylistClients);
+            return View(model);
+        }
+
+        [HttpPost("/stylists/{stylistId}/clients")]
+        public ActionResult Create(string clientName, int stylistId)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Stylist foundStylist = Stylist.Find(stylistId);
+            Client newClient = new Client(clientName, stylistId);
+            newClient.Save();
+            List<Client> stylistClients = foundStylist.GetClients();
+            model.Add("clients", stylistClients);
+            model.Add("stylist", foundStylist);
+            return View("Show", model);
+        }
+    }
+}
